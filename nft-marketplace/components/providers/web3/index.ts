d2fd5739c8c4c82081071db3dc612e1bb9b95b6e
Elar Saks <elarsaks@gmail.com>
2023-09-21 14:41:26 +0300

    Clean after merge.

--- nft-marketplace/components/providers/web3/index.ts
+++ nft-marketplace/components/providers/web3/index.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { createContext, useContext, useState, ReactNode } from "react";
 
 interface Web3Api {
@@ -13,25 +12,10 @@ const Web3Context = createContext<Web3Api | null>(null);
 
 const Web3Provider: React.FC<Web3ProviderProps> = ({ children }) => {
   const [web3Api] = useState<Web3Api>({ test: "Hello Provider!" });
-=======
-import { createContext, FunctionComponent, useContext, useState } from "react";
-
-const Web3Context = createContext<any>(null);
-
-const Web3Provider: FunctionComponent = ({ children }) => {
-  const [web3Api, setWeb3Api] = useState({ test: "Hello Provider!" });
->>>>>>> main
 
   return (
     <Web3Context.Provider value={web3Api}>{children}</Web3Context.Provider>
   );
 };
 
-<<<<<<< HEAD
-=======
-export function useWeb3() {
-  return useContext(Web3Context);
-}
-
->>>>>>> main
 export default Web3Provider;
