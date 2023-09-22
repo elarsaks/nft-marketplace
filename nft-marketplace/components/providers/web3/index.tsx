be496f27fcca38f5280109ee301353f5b261d019
Elar Saks <elarsaks@gmail.com>
2023-09-22 08:53:27 +0300

    Rename file to jsx.

---
+++ nft-marketplace/components/providers/web3/index.tsx
@@ -0,0 +1,21 @@
+import React, { createContext, useContext, useState, ReactNode } from "react";
+
+interface Web3ProviderProps {
+  children: ReactNode;
+}
+
+const Web3Context = React.createContext<any>(null);
+
+const Web3Provider: React.FC<Web3ProviderProps> = ({ children }) => {
+  const [web3Api, setWeb3Api] = useState({ test: "Hello Provider!" });
+
+  return (
+    <Web3Context.Provider value={web3Api}>{children}</Web3Context.Provider>
+  );
+};
+
+export function useWeb3() {
+  return useContext(Web3Context);
+}
+
+export default Web3Provider;
