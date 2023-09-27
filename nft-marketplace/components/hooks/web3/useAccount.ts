8ab5df03cd9f22aad68bec046a4dce169b6fb65b
Elar Saks <elarsaks@gmail.com>
2023-09-27 10:24:24 +0300

    Update types.

--- nft-marketplace/components/hooks/web3/useAccount.ts
+++ nft-marketplace/components/hooks/web3/useAccount.ts
@@ -2,16 +2,17 @@ import { CryptoHookFactory } from "@_types/hooks";
 import useSWR from "swr";
 
 // deps provider, ethereum, contract (web3Sate)
-export const hookFactory: CryptoHookFactory = (deps) => (params) => {
-  const swrRes = useSWR("web3/useAccount", () => {
-    console.log("deps", deps);
-    console.log("params", params);
-    // Making a request to get data
-    return "Test User";
-  });
+export const hookFactory: CryptoHookFactory<string, string> =
+  (deps) => (params) => {
+    const swrRes = useSWR("web3/useAccount", () => {
+      console.log("deps", deps);
+      console.log("params", params);
+      // Making a request to get data
+      return "Test User";
+    });
 
-  return swrRes;
-};
+    return swrRes;
+  };
 
 export const useAccount = hookFactory({
   ethereum: undefined,
--- nft-marketplace/types/hooks.ts
+++ nft-marketplace/types/hooks.ts
@@ -8,10 +8,13 @@ export type Web3Dependencies = {
   ethereum: MetaMaskInpageProvider;
 };
 
-export type CryptoSWRResponse = SWRResponse;
-
-export type CryptoHandlerHook = (params: string) => CryptoSWRResponse;
-
-export type CryptoHookFactory = {
-  (d: Partial<Web3Dependencies>): CryptoHandlerHook;
+export type CryptoHookFactory<D = any, P = any> = {
+  (d: Partial<Web3Dependencies>): CryptoHandlerHook<D, P>;
 };
+
+export type CryptoHandlerHook<D, P> = (params: P) => CryptoSWRResponse<D>;
+export type CryptoSWRResponse<D = any> = SWRResponse<D>;
+
+// export type CryptoHookFactory<D = any, P = any> = {
+//   (d: Partial<Web3Dependencies>): (params: P) => SWRResponse<D>;
+// };
