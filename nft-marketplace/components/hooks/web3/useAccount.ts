d95bfa6765e0abd1d6fafb1ad4de994ce8141c33
Elar Saks <elarsaks@gmail.com>
2023-09-28 07:45:16 +0300

    Conditional swr.

--- nft-marketplace/components/hooks/web3/useAccount.ts
+++ nft-marketplace/components/hooks/web3/useAccount.ts
@@ -1,16 +1,22 @@
 import { CryptoHookFactory } from "@_types/hooks";
 import useSWR from "swr";
 
-type AccountHookFactory = CryptoHookFactory<string, string>;
+type AccountHookFactory = CryptoHookFactory<string>;
 
 export type UseAccountHook = ReturnType<AccountHookFactory>;
 
 export const hookFactory: CryptoHookFactory<string, string> =
-  (deps) => (params) => {
+  ({ provider }) =>
+  (params) => {
     const swrRes = useSWR("web3/useAccount", () => {
-      console.log("deps", deps);
+      provider?.listAccounts();
+      console.log("deps", provider);
       console.log("params", params);
       // Making a request to get data
+      return "Test User";
+    });
+
+    useSWR(provider ? "web3/useAccount" : null, () => {
       return "Test User";
     });
 
