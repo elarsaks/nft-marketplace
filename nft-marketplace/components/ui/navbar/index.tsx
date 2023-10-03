74372fa986b3ffd41a14cc70ac7fd5d65c879b59
Elar Saks <elarsaks@gmail.com>
2023-10-03 15:26:01 +0300

    Add network hook.

--- nft-marketplace/components/ui/navbar/index.tsx
+++ nft-marketplace/components/ui/navbar/index.tsx
@@ -2,7 +2,7 @@
 
 import { Disclosure, Menu } from "@headlessui/react";
 import { BellIcon, MenuIcon, XIcon } from "@heroicons/react/outline";
-import { useAccount } from "@hooks/web3";
+import { useAccount, useNetwork } from "@hooks/web3";
 import ActiveLink from "../link";
 import Walletbar from "./Walletbar";
 
@@ -17,6 +17,9 @@ function classNames(...classes: string[]) {
 
 export default function Navbar() {
   const { account } = useAccount();
+  const { network } = useNetwork();
+
+  console.log(network.data);
 
   return (
     <Disclosure as="nav" className="bg-gray-800">
