91ef59d3186bc37a6113f22a3d07d17fffa7eb3d
Elar Saks <elarsaks@gmail.com>
2023-10-04 13:18:33 +0300

    Fix loading and undefined ethereum.

--- nft-marketplace/components/hooks/web3/useNetwork.ts
+++ nft-marketplace/components/hooks/web3/useNetwork.ts
@@ -49,6 +49,6 @@ export const hookFactory: NetworkHookFactory =
       isValidating,
       targetNetwork,
       isSupported: data === targetNetwork,
-      isLoading: isLoading || isValidating,
+      isLoading: isLoading as boolean,
     };
   };
