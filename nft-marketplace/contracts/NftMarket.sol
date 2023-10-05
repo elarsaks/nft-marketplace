64d425da7bd4ca2a4fbc68bbc577bbac79b9b049
Elar Saks <elarsaks@gmail.com>
2023-10-05 08:00:01 +0300

    Mint token.

--- nft-marketplace/contracts/NftMarket.sol
+++ nft-marketplace/contracts/NftMarket.sol
@@ -2,11 +2,25 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract NftMarket is ERC721URIStorage {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _listedItems;
+    Counters.Counter private _tokenIds;
+
     constructor() ERC721("CreaturesNFT", "CNFT") {}
 
-    function mint(address to, uint256 tokenId) public {
-        _mint(to, tokenId);
+    function mintToken(string memory tokenURI) public payable returns (uint) {
+        _tokenIds.increment();
+        _listedItems.increment();
+
+        uint newTokenId = _tokenIds.current();
+
+        _safeMint(msg.sender, newTokenId);
+        _setTokenURI(newTokenId, tokenURI);
+
+        return newTokenId;
     }
 }
